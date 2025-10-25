2c17cb2ee4d56855180ccd51bda81a5b88a1fc23
Gustavo <gustavoaraujomelo2003.gam@gmail.com>
2025-10-25 19:37:17 -0300

    Atualização de Página de Login

--- WEB/Login.js
+++ WEB/Login.js
@@ -1,14 +1,20 @@
-let botao = document.querySelector("button");
-
-hoverBotao = () => {
-    botao.style.boxShadow = '0px 5px 10px white'
-    botao.style.color = 'white'
+let form = document.querySelector('form');
+let botao = document.querySelector('button');
+botao.addEventListener('click', (Event) =>{
+    Event.preventDefault();
+let login = document.querySelector("input[type='text']").value;
+let senha = document.querySelector("input[type='password']").value;
+if(login === 'Gustavo' && senha === '1234') {
+    window.location.replace('Curriculo.html');
+} else {
+    alert('Por favor, preencha todos os campos!')
 }
-
-removeHoverBotao = () => {
+});
+let hoverBotao = () =>{
+    botao.style.boxShadow = '0px 10px 15px white'
+}
+let removeHoverBotao = () =>{
     botao.style.boxShadow = 'none'
-    botao.style.color = 'black'
 }
-
-botao.addEventListener("mouseover", hoverBotao);
-botao.addEventListener("mouseout", removeHoverBotao);
+botao.addEventListener('mouseover', hoverBotao);
+botao.addEventListener('mouseout', removeHoverBotao);
